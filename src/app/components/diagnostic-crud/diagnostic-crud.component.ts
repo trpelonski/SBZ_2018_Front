968fe02ea21e4ee73b646942f0f530e5312e9887
trpelonski <trpovski@gmail.com>
2018-06-16 03:52:03 +0200

    Uradjeni read i delete za dijagnoze

--- src/app/components/diagnostic-crud/diagnostic-crud.component.ts
+++ src/app/components/diagnostic-crud/diagnostic-crud.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CrudService } from '../../services/crud.service';
 
 @Component({
   selector: 'app-diagnostic-crud',
@@ -7,9 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiagnosticCrudComponent implements OnInit {
 
-  constructor() { }
+  dijagnozeStranica : number = 1;
+  diagnostics : any;
+
+  constructor(private crudService : CrudService) { }
 
   ngOnInit() {
+    this.ucitajDijagnoze();
+  }
+
+  ucitajDijagnoze(){
+    this.crudService.getDiagnostics(this.dijagnozeStranica).subscribe((res:any)=>{
+      if(res.success){
+        this.diagnostics = res.body.content;
+      }
+    })
+  }
+
+  deleteDiagnostic(id:number){
+    this.crudService.deleteDiagnostic(id).subscribe((res:any)=>{
+      if(res.success){
+        this.ucitajDijagnoze();
+      }
+    })
+  }
+
+  nextDiagnostic(){
+    this.crudService.getDiagnostics(this.dijagnozeStranica+1).subscribe((res:any)=>{
+      if(res.success){
+        if(res.body){
+          this.diagnostics = res.body.content;
+          this.dijagnozeStranica++;
+        }
+      }
+    })
+  }
+
+  prevDiagnostic(){
+    this.dijagnozeStranica--;
+    if(this.dijagnozeStranica <= 1){
+      this.dijagnozeStranica = 1;
+    }
+
+    this.ucitajDijagnoze();
   }
 
 }
